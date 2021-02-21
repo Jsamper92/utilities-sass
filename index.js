6fce98556ecaef624d24dd88952cb5c045b6f7f7
jorgesamper <jsamp94@outlook.com>
2021-02-21 21:17:13 +0100

    insert validation error creation partials

--- index.js
+++ index.js
@@ -78,85 +78,120 @@ const StyleDictionary = require("style-dictionary").extend({
 StyleDictionary.registerFormat({
   name: "custom/grid",
   formatter: (dictionary) => {
-    let result = [];
-    for (const key in dictionary.properties.grid) {
-      let value = dictionary.properties.grid[key];
-      layout = value.gutter.attributes.type;
-      const [gutter, offset, columns, width] = [
-        value.gutter,
-        value.offset,
-        value.columns,
-        value.width
-      ];
-      result += `${layout}:(
-          ${gutter.path[2]}:${gutter.value},
-          ${offset.path[2]}:${offset.value},
-          ${columns.path[2]}:${columns.value},
-          ${width.path[2]}:${width.value}
-        ),`;
-    }
+    try {
+      let result = [];
+      for (const key in dictionary.properties.grid) {
+        let value = dictionary.properties.grid[key];
+        layout = value.gutter.attributes.type;
+        const [gutter, offset, columns, width] = [
+          value.gutter,
+          value.offset,
+          value.columns,
+          value.width
+        ];
+        result += `${layout}:(
+            ${gutter.path[2]}:${gutter.value},
+            ${offset.path[2]}:${offset.value},
+            ${columns.path[2]}:${columns.value},
+            ${width.path[2]}:${width.value}
+          ),`;
+      }
 
-    return `/// Mapa creado dinamicamente en base al fichero de configuración. Define los puntos de ruptura de los distintos breakpoints\n/// @type map\n/// @group grid \n$breakpoints: (
-              ${result}
-          );`;
+      return `/// Mapa creado dinamicamente en base al fichero de configuración. Define los puntos de ruptura de los distintos breakpoints\n/// @type map\n/// @group grid \n$breakpoints: (
+                ${result}
+            );`;
+    } catch {
+      utils.errorConsole(
+        "No se ha especificado ninguna configuración de las utilidades de grid.El archivo se creará sin contenido. Por favor revisa el fichero de configuración .frontech.json"
+      );
+      return "";
+    }
   }
 });
 StyleDictionary.registerFormat({
   name: "custom/mediaqueries",
   formatter: (dictionary) => {
-    let result = [];
-    for (const key in dictionary.properties.grid) {
-      let value = dictionary.properties.grid[key];
-      layout = value.gutter.attributes.type;
-      const [width] = [value.width];
-      result += `/// Mixin cuyo objetivo es crear la media-query en base a los puntos de corte establecidos en el fichero de configuración\n///\n///\n/// @example scss\n///\n///      .test{\n///         width: 100%;\n///         @include screen-${key}(){\n///           width: auto;\n///         }\n///      }\n///\n/// @example css\n///\n///      .test {\n///         width: 100%;\n///       }\n///\n///      @media only screen and (min-width: ${width}) {\n///         .test {\n///           width: auto;\n///         }\n///      }\n///\n/// @group media-queries \n@mixin screen-${key}{
-            @media only screen and (min-width: ${width}) {
-              @content
-            }
-          };\n`;
-    }
+    try {
+      let result = [];
+      for (const key in dictionary.properties.grid) {
+        let value = dictionary.properties.grid[key];
+        layout = value.gutter.attributes.type;
+        const [width] = [value.width];
+        result += `/// Mixin cuyo objetivo es crear la media-query en base a los puntos de corte establecidos en el fichero de configuración\n///\n///\n/// @example scss\n///\n///      .test{\n///         width: 100%;\n///         @include screen-${key}(){\n///           width: auto;\n///         }\n///      }\n///\n/// @example css\n///\n///      .test {\n///         width: 100%;\n///       }\n///\n///      @media only screen and (min-width: ${width}) {\n///         .test {\n///           width: auto;\n///         }\n///      }\n///\n/// @group media-queries \n@mixin screen-${key}{
+              @media only screen and (min-width: ${width}) {
+                @content
+              }
+            };\n`;
+      }
 
-    return result;
+      return result;
+    } catch {
+      utils.errorConsole(
+        "No se ha especificado ninguna configuración de las utilidades de grid.El archivo se creará sin contenido. Por favor revisa el fichero de configuración .frontech.json"
+      );
+      return "";
+    }
   }
 });
 StyleDictionary.registerFormat({
   name: "custom/spacing",
   formatter: (dictionary) => {
-    const { increase, limit } = dictionary.properties.spacing;
+    try {
+      const { increase, limit } = dictionary.properties.spacing;
 
-    return `/// Mapa creado dinamicamente en base al fichero de configuración. Define los atributos para crear las clases de utilidad de margin y padding\n/// @type number\n/// @group spacing
-      $spacing: (
-          increase:${increase.value},
-          limit:${limit.value}
+      return `/// Mapa creado dinamicamente en base al fichero de configuración. Define los atributos para crear las clases de utilidad de margin y padding\n/// @type number\n/// @group spacing
+        $spacing: (
+            increase:${increase.value},
+            limit:${limit.value}
+        );
+      `;
+    } catch {
+      utils.errorConsole(
+        "No se ha especificado ninguna configuración de las utilidades de espacio.El archivo se creará sin contenido. Por favor revisa el fichero de configuración .frontech.json"
       );
-    `;
+      return "";
+    }
   }
 });
 StyleDictionary.registerFormat({
   name: "custom/properties-color",
   formatter: (dictionary) => {
-    let key = Object.keys(dictionary.properties.colors);
-    let customProperties = "\n";
-    key.forEach((item) => {
-      value = dictionary.properties.colors[item];
-      customProperties += `--${item}:${value.value};\n`;
-    });
-    return `/// Variables de color definida en el archivo .frontech.json\n///@group colors\n:root{${customProperties}};`;
+    try {
+      let key = Object.keys(dictionary.properties.colors);
+      let customProperties = "\n";
+      key.forEach((item) => {
+        value = dictionary.properties.colors[item];
+        customProperties += `--${item}:${value.value};\n`;
+      });
+      return `/// Variables de color definida en el archivo .frontech.json\n///@group colors\n:root{${customProperties}};`;
+    } catch {
+      utils.errorConsole(
+        "No se ha especificado ninguna configuración de las colores. El archivo se creará sin contenido. Por favor revisa el fichero de configuración .frontech.json"
+      );
+      return "";
+    }
   }
 });
 
 StyleDictionary.registerFormat({
   name: "custom/properties-typography",
   formatter: (dictionary) => {
-    let key = Object.keys(dictionary.properties.typography);
-    let fonts = "";
-    let customProperties = "";
-    key.forEach((font) => {
-      value = dictionary.properties.typography[font];
-      fonts += `\n${font}: (\nname:${value.family.value},\nweight:${value.weight.value},\nstyle:${value.style.value}\n),`;
-      customProperties += `--${font}:${value.family.value};\n`;
-    });
-    return `/// Mapa de fuentes definida en el archivo .frontech.json\n///@group fonts\n$fonts:(${fonts});\n\n/// Custom properties cuyo valor es el nombre aportado en el fichero .frontech.json\n/// @group fonts\n:root{\n${customProperties}};`;
+    try {
+      let key = Object.keys(dictionary.properties.typography);
+      let fonts = "";
+      let customProperties = "";
+      key.forEach((font) => {
+        value = dictionary.properties.typography[font];
+        fonts += `\n${font}: (\nname:${value.family.value},\nweight:${value.weight.value},\nstyle:${value.style.value}\n),`;
+        customProperties += `--${font}:${value.family.value};\n`;
+      });
+      return `/// Mapa de fuentes definida en el archivo .frontech.json\n///@group fonts\n$fonts:(${fonts});\n\n/// Custom properties cuyo valor es el nombre aportado en el fichero .frontech.json\n/// @group fonts\n:root{\n${customProperties}};`;
+    } catch {
+      utils.errorConsole(
+        "No se ha especificado ninguna configuración de tipografias. El archivo se creará sin contenido. Por favor revisa el fichero de configuración .frontech.json"
+      );
+      return "";
+    }
   }
 });
 
